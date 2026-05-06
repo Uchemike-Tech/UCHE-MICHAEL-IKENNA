5364e1eea9f584e01bcd826f2b148144d39f6a58
MICHAEL UCHE <uchemike069@gmail.com>
2026-05-06 07:15:04 +0100

    this

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { cloudflareVitePlugin } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    cloudflareVitePlugin(),
   ],
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
 });
