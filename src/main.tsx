20fc0d1664337ae3a99e91b4ea0c11b6924b2f9d
UCHE MICHAEL IKENNA <uchemike069@gmail.com>
2026-05-13 12:54:54 +0100

    chore: add missing entry point main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+import { hydrateRoot } from 'react-dom/client'
+import { StartClient } from '@tanstack/react-start'
+import { getRouter } from './router'
+
+const router = getRouter()
+
+hydrateRoot(document.getElementById('app')!, <StartClient router={router} />)
