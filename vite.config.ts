4f5b6bb81348aac2b4cf3fc559ea572b8a78228f
UCHE MICHAEL IKENNA <uchemike069@gmail.com>
2026-05-13 12:55:15 +0100

    chore: add TanStack Router Vite plugin to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite(),
     react(),
     tsconfigPaths(),
   ],
